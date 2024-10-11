7764e99aa479d04da72ed9cfff932948e575a77b
Brinley Ostler <brinley.ostler@gmail.com>
2024-10-11 11:46:32 -0600

    add penalized regression model

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -30,28 +30,52 @@ action_bar + title_hist
 #### FEATURE ENGINEERING ####
 amazon_recipe <- recipe(ACTION ~., data=amazon_train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
-  step_other(all_nominal_predictors(), threshold = .1) %>% # combines categorical values that occur
-  step_dummy(all_nominal_predictors()) %>% # dummy variable encoding
+  step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% 
   step_normalize(all_nominal_predictors()) #target encoding (must
 # also step_lencode_glm() and step_lencode_bayes()
 
 amazon_prepped = prep(amazon_recipe)
 baked <- bake(amazon_prepped, new_data = amazon_train)
 #
-#### LOGISTIC REGRESSION ####
-log_reg_model <- logistic_reg() %>% 
-  set_engine("glm")
+#### PENALIZED REGRESSION ####
+pen_reg_model <- logistic_reg(penalty=tune(),
+                              mixture=tune()) %>% 
+  set_engine("glmnet")
 
 ## Put into a workflow
-log_reg_workflow <- workflow() %>% 
+pen_reg_workflow <- workflow() %>% 
   add_recipe(amazon_recipe) %>% 
-  add_model(log_reg_model) %>% 
+  add_model(pen_reg_model)
+
+## Grid of values to tune over
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels=5)
+
+#### CV ####
+## Split data for CV
+folds <- vfold_cv(amazon_train, v=5, repeats=1)
+
+## Run the CV
+CV_results <- pen_reg_workflow %>% 
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
+# metric_set(roc_auc, f_meas, sens, recall, spec, precision, accuracy)
+
+## Find best tuning parameters
+best_tune <- CV_results %>% 
+  select_best(metric="roc_auc")
+
+#### Finalize the workflow and fit it ####
+final_wf <- pen_reg_workflow %>% 
+  finalize_workflow(best_tune) %>% 
   fit(data=amazon_train)
 
-## Make predictions
-amazon_preds <- predict(log_reg_workflow,
-                              new_data = amazon_test,
-                              type="prob")
+#### Make predictions ####
+amazon_preds <- final_wf %>% 
+  predict(new_data = amazon_test, type="prob")
 
 ## Format predictions for kaggle upload
 recipe_kaggle_submission <- amazon_preds %>% 
@@ -60,6 +84,6 @@ recipe_kaggle_submission <- amazon_preds %>%
   select(id, ACTION)
 
 ## Write out file
-vroom_write(x=recipe_kaggle_submission, file="./LogRegPreds.csv", delim=",")
+vroom_write(x=recipe_kaggle_submission, file="./PRegPreds.csv", delim=",")
 
 
