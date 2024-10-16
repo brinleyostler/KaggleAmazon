12c5c0ec0351ce8e9cc02c635858b8e6993ac96c
Brinley Ostler <brinley.ostler@gmail.com>
2024-10-16 09:44:42 -0600

    add KNN model

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -12,41 +12,7 @@ amazon_train = vroom("./train.csv")
 ## CLEAN THE DATA
 amazon_train$ACTION = factor(amazon_train$ACTION) #factor response variable
 #
-#### LOGISTIC REGRESSION ####
-logreg_recipe <- recipe(ACTION ~., data=amazon_train) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
-  step_other(all_nominal_predictors(), threshold = .1) %>% # combines categorical values that occur
-  step_dummy(all_nominal_predictors()) %>% # dummy variable encoding
-  step_normalize(all_nominal_predictors()) #target encoding (must
-# also step_lencode_glm() and step_lencode_bayes()
-
-amazon_prepped = prep(logreg_recipe)
-baked <- bake(amazon_prepped, new_data = amazon_train)
-
-log_reg_model <- logistic_reg() %>% 
-  set_engine("glm")
-
-## Put into a workflow
-log_reg_workflow <- workflow() %>% 
-  add_recipe(logreg_recipe) %>% 
-  add_model(log_reg_model) %>% 
-  fit(data=amazon_train)
-
-## Make predictions
-amazon_preds <- predict(log_reg_workflow,
-                        new_data = amazon_test,
-                        type="prob")
-
-## Format predictions for kaggle upload
-recipe_kaggle_submission <- amazon_preds %>% 
-  bind_cols(., amazon_test) %>% 
-  rename(ACTION=.pred_1) %>% 
-  select(id, ACTION)
-
-## Write out file
-vroom_write(x=recipe_kaggle_submission, file="../../LogRegPreds.csv", delim=",")
-
-#### PENALIZED REGRESSION ####
+#### K NEAREST NEIGHBORS ####
 #### FEATURE ENGINEERING ####
 amazon_recipe <- recipe(ACTION ~., data=amazon_train) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
@@ -58,18 +24,17 @@ amazon_recipe <- recipe(ACTION ~., data=amazon_train) %>%
 amazon_prepped = prep(amazon_recipe)
 baked <- bake(amazon_prepped, new_data = amazon_train)
 
-pen_reg_model <- logistic_reg(penalty=tune(),
-                              mixture=tune()) %>% 
-  set_engine("glmnet")
+knn_model <- nearest_neighbor(neighbors=tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("kknn")
 
 ## Put into a workflow
-pen_reg_workflow <- workflow() %>% 
+knn_workflow <- workflow() %>% 
   add_recipe(amazon_recipe) %>% 
-  add_model(pen_reg_model)
+  add_model(knn_model)
 
 ## Grid of values to tune over
-tuning_grid <- grid_regular(penalty(),
-                            mixture(),
+tuning_grid <- grid_regular(neighbors(),
                             levels=5)
 
 #### CV ####
@@ -77,7 +42,7 @@ tuning_grid <- grid_regular(penalty(),
 folds <- vfold_cv(amazon_train, v=5, repeats=1)
 
 ## Run the CV
-CV_results <- pen_reg_workflow %>% 
+CV_results <- knn_workflow %>% 
   tune_grid(resamples=folds,
             grid=tuning_grid,
             metrics=metric_set(roc_auc))
@@ -88,7 +53,7 @@ best_tune <- CV_results %>%
   select_best(metric="roc_auc")
 
 #### Finalize the workflow and fit it ####
-final_wf <- pen_reg_workflow %>% 
+final_wf <- knn_workflow %>% 
   finalize_workflow(best_tune) %>% 
   fit(data=amazon_train)
 
@@ -103,6 +68,6 @@ recipe_kaggle_submission <- amazon_preds %>%
   select(id, ACTION)
 
 ## Write out file
-vroom_write(x=recipe_kaggle_submission, file="../../PRegPreds.csv", delim=",")
+vroom_write(x=recipe_kaggle_submission, file="KNNPreds.csv", delim=",")
 
 
